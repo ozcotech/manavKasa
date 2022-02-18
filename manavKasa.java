2a04c8f7f0750850b7f626eeae8c97c656d5b37e
ozcotech <ozkancomert@aof.anadolu.edu.tr>
2022-02-18 22:47:25 +0300

    manaKasa.java file added.

---
+++ manavKasa.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class manavKasa{
+
+  public static void main(String[] args){
+
+  double armut = 2.14; elma = 3.67; domates = 1.11; muz = 0.95; patlican = 5.00;
+
+  Scanner kacKilo = new Scanner(System.in);
+
+
+
+
+  }
+
+}
