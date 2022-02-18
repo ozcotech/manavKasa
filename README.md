103b1af59fafc098f1921f519a624f2a22ecf9a2
ozcotech <ozkancomert@aof.anadolu.edu.tr>
2022-02-18 23:31:38 +0300

    readme file filled.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Manav Kasa Programı 
+
+### Manav Kasa Basit Java Programı
+* Patika Ödevi
+* armut, elma, domates, muz, patlıcan
+  
--- manavKasa.java
+++ manavKasa.java
@@ -4,12 +4,30 @@ public class manavKasa{
 
   public static void main(String[] args){
 
-  double armut = 2.14; elma = 3.67; domates = 1.11; muz = 0.95; patlican = 5.00;
+  double armut, elma, domates, muz, patlican, toplam;
 
   Scanner kacKilo = new Scanner(System.in);
 
+  System.out.print(" Kaç Kilo Armut Aldınız: ");
+  armut = kacKilo.nextInt();
+  
+  System.out.print(" Kaç Kilo Elma Aldınız: ");
+  elma = kacKilo.nextInt();
 
+  System.out.print(" Kaç Kilo Domates Aldınız: ");
+  domates = kacKilo.nextInt();
 
+  System.out.print(" Kaç Kilo Muz Aldınız: ");
+  muz = kacKilo.nextInt();
+
+  System.out.print(" Kaç Kilo Patlican Aldınız: ");
+  patlican = kacKilo.nextInt();
+
+  toplam = (armut * 2.14) + (elma * 3.67) + (domates * 1.11) + (muz * 0.95) + (patlican * 5);
+
+  System.out.println("Toplam Tutar :" + toplam + "TL");
+
+  kacKilo.close();
 
   }
 
